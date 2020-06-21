3a054e3a7cbffec813f386caf2a9318e456cf9a9
Shahed Talukder <shahedtalukder51@gmail.com>
2020-06-21 20:20:53 +0600

    shahed

---
+++ for_loop.py
@@ -0,0 +1,20 @@
+n = int(input("Enter an integer number:"))
+# for sazid in range(1,n+1,2):
+#     print(sazid)
+
+# for i in range(1,n+1):
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         pass
+
+i = 1 #this is the initial point
+while(i <= 10): #making condition
+    # statement part start
+    if i % 2 != 0:
+        print(i)
+    else:
+        pass
+    # statement part end
+    # increment part
+    i+=1
---
+++ while_loop.py
@@ -0,0 +1,11 @@
+
+i = 1 #this is the initial point
+while(i <= 10): #making condition
+    # statement part start
+    if i % 2 != 0:
+        print(i)
+    else:
+        pass
+    # statement part end
+    # increment part
+    i+=1
